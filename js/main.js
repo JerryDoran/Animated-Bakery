f7459e413361e14c2c45f90f4ea565a33da32150
Jerry Doran <Jerry.Doran@pccairfoils.com>
2019-11-01 13:18:35 -0400

    Finished Bakery Project

--- js/main.js
+++ js/main.js
@@ -2,3 +2,9 @@ $('.hamburger-menu').on('click', () => {
   $('.toggle').toggleClass('open');
   $('.nav-list').toggleClass('open');
 });
+
+// ANIMATIONS
+AOS.init({
+  easing: 'ease',
+  duration: 1000
+});
